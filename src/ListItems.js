c520c6866299605ebb840e447d70987427786705
Clara <everettclara8@gmail.com>
2020-04-17 10:04:43 -0400

    Changed Import

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ListItems.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function ListItems(props){
   const items = props.items;
